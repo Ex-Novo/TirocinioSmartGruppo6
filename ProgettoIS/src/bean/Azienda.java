8de13ad4a9aaa4f0a2787c897a9a79f0416092a5
Mario <silverx.skillato@gmail.com>
2017-12-27 17:56:25 +0100

    Aggiunta bean fatti da Anna

---
+++ ProgettoIS/src/bean/Azienda.java
@@ -0,0 +1,50 @@
+package bean;
+
+public class Azienda {
+	private String p_iva;
+	private String nomeAzienda;
+	private String tutorAziendale;
+	private String email;
+	private String password;
+	private String telefono;
+	
+	
+	public String getP_iva() {
+		return p_iva;
+	}
+	public void setP_iva(String p_iva) {
+		this.p_iva = p_iva;
+	}
+	public String getNomeAzienda() {
+		return nomeAzienda;
+	}
+	public void setNomeAzienda(String nomeAzienda) {
+		this.nomeAzienda = nomeAzienda;
+	}
+	public String getTutorAziendale() {
+		return tutorAziendale;
+	}
+	public void setTutorAziendale(String tutorAziendale) {
+		this.tutorAziendale = tutorAziendale;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+}
---
+++ ProgettoIS/src/bean/Didattica.java
@@ -0,0 +1,42 @@
+package bean;
+
+public class Didattica {
+	
+	private String email;
+	private String nome;
+	private String cognome;
+	private String password;
+	private int tipo;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+}
