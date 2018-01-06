b3125aea9c08496b6faf01c614d3c737bdc32b40
Francesco D'Auria <dauriafrancescomaria@gmail.com>
2018-01-06 18:40:50 +0100

    Control per visualizzare la lista delle RichiesteTirocinio/Convenzione

---
+++ ProgettoIS/src/control/ViewListaRichiesteControl.java
@@ -0,0 +1,75 @@
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Azienda;
+import bean.Convenzione;
+import bean.RichiestaTirocinio;
+import bean.Studente;
+import dao.AziendaDaoImpl;
+import dao.AziendaDaoInterface;
+import dao.ConvenzioneDaoImpl;
+import dao.RichiestaTirocinioDaoImpl;
+import dao.StudenteDaoImpl;
+import dao.StudenteDaoInterface;
+
+/**
+ * Servlet usata per mandare alla jsp le richieste di tirocinio e di convenzione da visualizzare
+ */
+@WebServlet("/RichiestaTirocinioControl")
+public class ViewListaRichiesteControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewListaRichiesteControl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doPost(request, response);
+	}
+
+	/**
+	 * Chiama due metodi dao per ricavare la lista completa di tutti gli studenti e aziende registrate al sistema
+	 * 
+	 * @author: Francesco D'Auria, Luca Lamberti
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		RichiestaTirocinioDaoImpl richiesteTirocinioDAO = new RichiestaTirocinioDaoImpl();
+		ConvenzioneDaoImpl richiesteConvenzioneDAO = new ConvenzioneDaoImpl();
+		
+		// Metodi DAO usati per ottenere gli ArrayList di richieste
+		ArrayList<RichiestaTirocinio> listaRichiesteTirocinio = richiesteTirocinioDAO.getRichiesteTirocinio();
+		ArrayList<Convenzione> listaRichiesteConvenzione = richiesteConvenzioneDAO.getRichiesteConvenzione();
+		
+		// Invio alle .jsp degli ArrayList tramite attributi
+		out.println("Numero richieste tirocinio =" + listaRichiesteTirocinio.size());
+		request.setAttribute("listaRichiesteTirocinio", listaRichiesteTirocinio);
+		
+		out.println("Numero richieste convenzione =" + listaRichiesteConvenzione.size());
+		request.setAttribute("listaRichiesteTirocinio", listaRichiesteConvenzione);
+		
+		/* Reindirizza alla jsp dove verrà visualizzata la lista */
+		getServletConfig().getServletContext().getRequestDispatcher("/ListaRichieste.jsp").forward(request, response);
+		
+	}
+
+}
