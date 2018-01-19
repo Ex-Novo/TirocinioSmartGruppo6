b2c40036be516d43418d7d6b925412ae93fcd980
Francesco D'Auria <dauriafrancescomaria@gmail.com>
2018-01-19 16:43:48 +0100

    Checkstyle di Gestione Feedback

--- ProgettoIS/src/control/gestioneFeedback/CompilaRegistroControl.java
+++ ProgettoIS/src/control/gestioneFeedback/CompilaRegistroControl.java
@@ -1,18 +1,9 @@
 package control.gestioneFeedback;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import bean.Attivita;
 import bean.Registro;
 import bean.RichiestaTirocinio;
+
 import dao.AttivitaDaoImpl;
 import dao.AttivitaDaoInterface;
 import dao.RegistroDaoImpl;
@@ -20,89 +11,100 @@ import dao.RegistroDaoInterface;
 import dao.RichiestaTirocinioDaoImpl;
 import dao.RichiestaTirocinioDaoInterface;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
 /**
- * Servlet implementation class CompilaRegistroControl
+ * Servlet implementation class CompilaRegistroControl.
  */
+
 @WebServlet("/CompilaRegistroControl")
 public class CompilaRegistroControl extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CompilaRegistroControl() {
+  private static final long serialVersionUID = 1L;
+
+  
+  
+  public CompilaRegistroControl() {
         super();
-        // TODO Auto-generated constructor stub
+       
+  }
+
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request,response);
+  }
+
+    /**
+   * Il metodo prende in input i dati dell'attività da registrare
+   * dal form e la richiamo i dao per salvarla nel database.
+   * 
+   * @author Mario Procida.
+   */
+  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    PrintWriter out = response.getWriter();
+    
+    HttpSession session = request.getSession();
+    
+    String tipo = request.getParameter("tipo");
+    
+    
+    if (tipo.equals("addAtt")) {
+      String matricola = request.getParameter("matricola");
+      session.setAttribute("matricolaStudente", matricola);
+      response.sendRedirect("CompilaRegistro.jsp");
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request,response);
-	}
-
-	/**
-	 * Il metodo prende in input i dati dell'attività da registrare dal form e la richiamo i dao per salvarla nel database
-	 * 
-	 * @author Mario Procida.
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter out = response.getWriter();
-		
-		HttpSession session = request.getSession();
-		
-		String tipo = request.getParameter("tipo");
-		
-		
-		if(tipo.equals("addAtt")) {
-			String matricola = request.getParameter("matricola");
-			session.setAttribute("matricolaStudente", matricola);
-			response.sendRedirect("CompilaRegistro.jsp");
-		}
-		
-		if(tipo.equals("sendAtt")) {
-			
-			String matricolaStudente = (String) session.getAttribute("matricolaStudente");
-			String data = request.getParameter("data");
-			int oreLavoro = Integer.parseInt(request.getParameter("oreLavoro"));
-			String 	descrizione = request.getParameter("descrizione");
-			
-			RichiestaTirocinioDaoInterface richTirDao = new RichiestaTirocinioDaoImpl();
-			RichiestaTirocinio richTir = richTirDao.getRichTirocinio(matricolaStudente);
-			
-			RegistroDaoInterface regDao = new RegistroDaoImpl();
-			Registro reg = regDao.getRegistroByMatricola(matricolaStudente);
-			
-			Attivita att = new Attivita();
-			att.setData(data);
-			att.setOre(oreLavoro);
-			att.setDescrizione(descrizione);
-			att.setIdTirocinio(richTir.getIdTirocinio());
-			att.setIdRegistro(reg.getIdRegistro());
-			
-			AttivitaDaoInterface attDao = new AttivitaDaoImpl();
-			
-			boolean result = attDao.inserisciAttivita(att);
-			
-			
-			if(result) {
-				out.println("<script>");
-				out.println("alert('Attivita registrata con successo')");
-				out.println("window.open('index.jsp','_self')");
-				out.println("</script>");
-			}else {
-				out.println("<script>");
-				out.println("alert('Non e stato possibile aggiungere l attivita')");
-				out.println("window.history.back()");
-				out.println("</script>");
-			}
-		}
-		
-		
-		out.flush();
-		out.close();
-	}
+    
+    if (tipo.equals("sendAtt")) {
+      
+      String matricolaStudente = (String) session.getAttribute("matricolaStudente");
+      String data = request.getParameter("data");
+      int oreLavoro = Integer.parseInt(request.getParameter("oreLavoro"));
+      String  descrizione = request.getParameter("descrizione");
+      
+      RichiestaTirocinioDaoInterface richTirDao = new RichiestaTirocinioDaoImpl();
+      RichiestaTirocinio richTir = richTirDao.getRichTirocinio(matricolaStudente);
+      
+      RegistroDaoInterface regDao = new RegistroDaoImpl();
+      Registro reg = regDao.getRegistroByMatricola(matricolaStudente);
+      
+      Attivita att = new Attivita();
+      att.setData(data);
+      att.setOre(oreLavoro);
+      att.setDescrizione(descrizione);
+      att.setIdTirocinio(richTir.getIdTirocinio());
+      att.setIdRegistro(reg.getIdRegistro());
+      
+      AttivitaDaoInterface attDao = new AttivitaDaoImpl();
+      
+      boolean result = attDao.inserisciAttivita(att);
+      
+      
+      if (result) {
+        out.println("<script>");
+        out.println("alert('Attivita registrata con successo')");
+        out.println("window.open('index.jsp','_self')");
+        out.println("</script>");
+      } else {
+        out.println("<script>");
+        out.println("alert('Non e stato possibile aggiungere l attivita')");
+        out.println("window.history.back()");
+        out.println("</script>");
+      }
+    }
+    
+    
+    out.flush();
+    out.close();
+  }
 
 }
--- ProgettoIS/src/control/gestioneFeedback/FeedBackStudenteControl.java
+++ ProgettoIS/src/control/gestioneFeedback/FeedBackStudenteControl.java
@@ -1,18 +1,5 @@
 package control.gestioneFeedback;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import bean.Feedback;
 import bean.Tirocinio;
 import dao.FeedBackDaoImpl;
@@ -20,96 +7,100 @@ import dao.FeedBackDaoInterface;
 import dao.TirocinioDaoImpl;
 import dao.TirocinioDaoInterface;
 
-/**
- * Servlet implementation class FeedBackAziendaControl
- */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @WebServlet("/FeedBackStudenteControl")
 public class FeedBackStudenteControl extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FeedBackStudenteControl() {
+  private static final long serialVersionUID = 1L;
+
+  public FeedBackStudenteControl() {
         super();
-        // TODO Auto-generated constructor stub
+  }
+
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+  /**
+   * Il metodo prende le valutazioni dell'utente e fa la media.
+   * Infine chiama i metodi dao per salvare il feedback nel database.
+   * @author Mario Procida, Luca Lamberti
+   */
+  
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    HttpSession session = request.getSession();
+    
+    String piva = (String) session.getAttribute("piva");
+    String tipo = request.getParameter("tipo");
+    
+    if (tipo.equals("sendValutazione")) {
+      
+      String matricola = request.getParameter("matricola");
+      session.setAttribute("matricolaStudente", matricola);
+      session.setAttribute("tipoVal", "fbStudente");
+      
+      getServletConfig().getServletContext()
+      .getRequestDispatcher("/checkVal").forward(request, response);
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doPost(request, response);
-	}
-
-	/**
-	 * Il metodo prende le valutazioni dell'utente e fa la media. Infine chiama i metodi dao per salvare il feedback nel database.
-	 * @author Mario Procida, Luca Lamberti
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session = request.getSession();
-		
-		String piva = (String) session.getAttribute("piva");
-		String tipo = request.getParameter("tipo");
-		
-		if(tipo.equals("sendValutazione")) {
-			
-			String matricola = request.getParameter("matricola");
-			session.setAttribute("matricolaStudente", matricola);
-			session.setAttribute("tipoVal", "fbStudente");
-			
-			getServletConfig().getServletContext().getRequestDispatcher("/checkVal").forward(request, response);
-		}
-		
-		if(tipo.equals("confermaForm")) {
-			
-			double rating1 = Double.parseDouble(request.getParameter("rating1"));
-			double rating2 = Double.parseDouble(request.getParameter("rating2"));
-			double rating3 = Double.parseDouble(request.getParameter("rating3"));
-			
-			double finalRating = (rating1 + rating2 + rating3) / 3; //calcolo media valutazione
-			
-			TirocinioDaoInterface tirDao = new TirocinioDaoImpl();
-			Tirocinio tir = tirDao.getDettagliAziendeConvenzionate(piva);
-			
-			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-			Date date = new Date();
-			
-			Feedback fb = new Feedback();
-			fb.setPiva(piva);
-			fb.setMatricola((String)session.getAttribute("matricolaStudente"));
-			fb.setValutazioneStudente(finalRating);
-			fb.setIdTirocinio(tir.getIdTirocinio());
-			fb.setData(dateFormat.format(date));
-			
-			FeedBackDaoInterface fbDao = new FeedBackDaoImpl();
-			boolean result = fbDao.inviaFeedBackStudente(fb);
-			
-			PrintWriter out = response.getWriter();
-			//se la query è andata a buon fine
-			if(result){
-					
-				session.removeAttribute("matricolaStudente");
-				session.setAttribute("canFeed", false);
-				out.println("<script>");
-				out.println("alert('Valutazione inviata con successo')");
-				out.println("window.open('index.jsp','_self')");
-				out.println("</script>");
-				
-			}
-			//se la query non è andata a buon fine
-			else{
-				out.println("<script>");
-				out.println("alert('Non è stato possibile inviare la valutazione')");
-				out.println("window.history.back()");
-				out.println("</script>");
-				
-			}
-		}
-		
-		
-	}
+    
+    if (tipo.equals("confermaForm")) {
+      
+      double rating1 = Double.parseDouble(request.getParameter("rating1"));
+      double rating2 = Double.parseDouble(request.getParameter("rating2"));
+      double rating3 = Double.parseDouble(request.getParameter("rating3"));
+      
+      double finalRating = (rating1 + rating2 + rating3) / 3; //calcolo media valutazione
+      
+      TirocinioDaoInterface tirDao = new TirocinioDaoImpl();
+      Tirocinio tir = tirDao.getDettagliAziendeConvenzionate(piva);
+      
+      DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+      Date date = new Date();
+      
+      Feedback fb = new Feedback();
+      fb.setPiva(piva);
+      fb.setMatricola((String)session.getAttribute("matricolaStudente"));
+      fb.setValutazioneStudente(finalRating);
+      fb.setIdTirocinio(tir.getIdTirocinio());
+      fb.setData(dateFormat.format(date));
+      
+      FeedBackDaoInterface fbDao = new FeedBackDaoImpl();
+      boolean result = fbDao.inviaFeedBackStudente(fb);
+      
+      PrintWriter out = response.getWriter();
+      
+      //se la query è andata a buon fine
+      if (result) {
+          
+        session.removeAttribute("matricolaStudente");
+        session.setAttribute("canFeed", false);
+        out.println("<script>");
+        out.println("alert('Valutazione inviata con successo')");
+        out.println("window.open('index.jsp','_self')");
+        out.println("</script>");
+        
+      } else { //se la query non è andata a buon fine
+        out.println("<script>");
+        out.println("alert('Non è stato possibile inviare la valutazione')");
+        out.println("window.history.back()");
+        out.println("</script>");
+        
+      }
+    }
+    
+    
+  }
 
 }
